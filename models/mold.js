0fc7176057c568aff618b3dbc4c83f256b45b589
John <john.hardberger@gmail.com>
2018-12-10 18:04:28 -0600

    added default values to mold model

--- models/mold.js
+++ models/mold.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const MoldSchema = new mongoose.Schema({
 	title: String,
 	seed_song_id: String,
-	acoustic: Boolean,
-	danceable: Boolean,
-	energetic: Boolean,
-	instrumental: Boolean,
-	live: Boolean,
-	spoken: Boolean,
-	upbeat: Boolean,
+	acoustic: {type: Boolean, default: true},
+	danceable: {type: Boolean, default: true},
+	energetic: {type: Boolean, default: true},
+	instrumental: {type: Boolean, default: true},
+	live: {type: Boolean, default: true},
+	spoken: {type: Boolean, default: true},
+	upbeat: {type: Boolean, default: true},
 });
 
 module.exports = mongoose.model('Mold', MoldSchema);
